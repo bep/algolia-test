623f718bb116599b854347b61e142ee1957e0761
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2020-05-05 23:49:41 +0200

    Code cleaning

--- assets/js/hs-app.js
+++ assets/js/hs-app.js
@@ -1,70 +1,65 @@
 function initSearch(cfg) {
-  return {
-    query: '',
-    loading: true,
-    results: null,
-    search: debounce(function () {
-      if (!this.query) {
-        return;
-      }
+	return {
+		query: '',
+		loading: true,
+		results: null,
+		search: function() {
+			if (!this.query) {
+				return;
+			}
 
-      this.loading = true;
+			this.loading = true;
 
-   
-      var queries = {
-        requests:
-          [
-            { indexName: cfg.index, facets: ["genre"], params: `query=${encodeURIComponent(this.query)}` }
-          ]
-      };
+			var queries = {
+				requests: [
+					{ indexName: cfg.index, facets: [ 'genre' ], params: `query=${encodeURIComponent(this.query)}` }
+				]
+			};
 
-      const host = `https://${cfg.app_id}-dsn.algolia.net`;
-      const url = `${host}/1/indexes/*/queries`;
+			const host = `https://${cfg.app_id}-dsn.algolia.net`;
+			const url = `${host}/1/indexes/*/queries`;
 
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'X-Algolia-Application-Id': cfg.app_id,
-          'X-Algolia-API-Key': cfg.api_key,
-        },
-        body: JSON.stringify(queries)
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          this.results = null;
-          if (data.results) {
-            this.results = {
-              first: data.results[0],
-            };
+			fetch(url, {
+				method: 'POST',
+				headers: {
+					'X-Algolia-Application-Id': cfg.app_id,
+					'X-Algolia-API-Key': cfg.api_key
+				},
+				body: JSON.stringify(queries)
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					this.results = null;
+					if (data.results) {
+						this.results = {
+							first: data.results[0]
+						};
 
-            console.log("First",data.results[0]);
+						var event = new CustomEvent('searchresult', {
+							bubbles: true,
+							cancelable: true,
+							detail: this.results
+						});
 
-            var event = new CustomEvent('searchresult', {
-              bubbles: true,
-              cancelable: true,
-              detail: this.results,
-            });
-            document.dispatchEvent(event);
+						document.dispatchEvent(event);
+					}
 
-          }
-
-
-          this.loading = false;
-        });
-    }, 250)
-  };
+					this.loading = false;
+				});
+		}
+	};
 }
 
-
 function debounce(func, wait) {
-  var timeout
-  return function () {
-      var context = this, args = arguments
-      var later = function () {
-          timeout = null
-          func.apply(context, args)
-      }
-      clearTimeout(timeout)
-      timeout = setTimeout(later, wait)
-  }
+	var timeout;
+	return function() {
+		var context = this,
+			args = arguments;
+		var later = function() {
+			timeout = null;
+			func.apply(context, args);
+		};
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	};
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com:bep/algolia-test
 go 1.13
 
 require (
+	github.com/alpinejs/alpine v2.3.3+incompatible // indirect
 	github.com/bep/hugo-jslibs/alpinejs v0.4.0 // indirect
 	github.com/bep/hugo-jslibs/turbolinks v0.1.0 // indirect
 	github.com/gohugoio/hugo-mod-bootstrap-scss-v4 v0.0.0-20200224171633-fc89315b7997 // indirect
